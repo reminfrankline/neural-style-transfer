ea7414fcc552d36305de5ee98a62c25cf7ffea36
Remin Franklin Eliyas <115979052+reminfrankline@users.noreply.github.com>
2024-05-27 20:10:51 +0100

    Add files via upload

---
+++ neural_style_transfer.py
@@ -0,0 +1,176 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import tensorflow_hub as hub
+
+def load_image(image_path, max_dim=512):
+  img = PIL.Image.open(image_path)
+  img = img.convert('RGB')
+  long = max(img.size)
+  scale = max_dim / long
+  img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), PIL.Image.ANTIALIAS)
+  img = np.array(img)
+
+  img = np.expand_dims(img, axis=0)
+  img = tf.keras.applications.vgg19.preprocess_input(img)
+  return img
+
+def preprocess_image(image):
+  x = image.copy()
+  if len(x.shape) == 4:
+    x = np.squeeze(x, 0)
+  assert len(x.shape) == 3
+
+
+  x[:, :, 0] += 103.939
+  x[:, :, 1] += 116.779
+  x[:, :, 2] += 123.68
+  x = x[:, :, ::-1]
+
+  x = np.clip(x, 0, 255).astype('uint8')
+  return x
+
+vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+vgg.trainable = False
+
+content_layers = ['block5_conv2']
+
+style_layers = [
+    'block1_conv1',
+    'block2_conv1',
+    'block3_conv1',
+    'block4_conv1',
+    'block5_conv1',
+]
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+def get_model():
+  vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+  vgg.trainable = False
+
+  style_outputs = [vgg.get_layer(name).output for name in style_layers]
+  content_outputs = [vgg.get_layer(name).output for name in content_layers]
+  model_outputs = style_outputs + content_outputs
+
+  return tf.keras.Model(vgg.input, model_outputs)
+
+def get_content_loss(base_content, target):
+  return tf.reduce_mean(tf.square(base_content - target))
+
+def gram_matrix(input_tensor):
+    channels = int(input_tensor.shape[-1])
+    a = tf.reshape(input_tensor, [-1, channels])
+    n = tf.shape(a)[0]
+    gram = tf.matmul(a, a, transpose_a=True)
+    return gram / tf.cast(n, tf.float32)
+
+def get_style_loss(base_style, gram_target):
+    gram_style = gram_matrix(base_style)
+    return tf.reduce_mean(tf.square(gram_style - gram_target))
+
+def compute_loss(model, loss_weights, init_image, gram_style_features, content_features):
+    style_weight, content_weight = loss_weights
+
+    model_outputs = model(init_image)
+
+    style_output_features = model_outputs[:num_style_layers]
+    content_output_features = model_outputs[num_style_layers:]
+
+    style_score = 0
+    content_score = 0
+
+    weight_per_style_layer = 1.0 / float(num_style_layers)
+    for target_style, comb_style in zip(gram_style_features, style_output_features):
+        style_score += weight_per_style_layer * get_style_loss(comb_style[0], target_style)
+
+    weight_per_content_layer = 1.0 / float(num_content_layers)
+    for target_content, comb_content in zip(content_features, content_output_features):
+        content_score += weight_per_content_layer * get_content_loss(comb_content[0], target_content)
+
+    style_score *= style_weight
+    content_score *= content_weight
+
+    loss = style_score + content_score
+    return loss, style_score, content_score
+
+@tf.function()
+def compute_grads(cfg):
+    with tf.GradientTape() as tape:
+        all_loss = compute_loss(**cfg)
+
+    total_loss = all_loss[0]
+    return tape.gradient(total_loss, cfg['init_image']), all_loss
+
+def deprocess_image(processed_img):
+    x = processed_img.copy()
+    if len(x.shape) == 4:
+        x = np.squeeze(x, 0)
+    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                               "dimension [1, height, width, channel] or [height, width, channel]")
+
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
+
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+def run_style_transfer(content_path, style_path, num_iterations=1000, content_weight=1e3, style_weight=1e-2):
+
+    content_image = load_image(content_path)
+    style_image = load_image(style_path)
+
+    model = get_model()
+    for layer in model.layers:
+        layer.trainable = False
+
+    style_features = [model(style_image)[i] for i in range(num_style_layers)]
+    content_features = [model(content_image)[i + num_style_layers] for i in range(num_content_layers)]
+    gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
+
+    init_image = tf.Variable(content_image, dtype=tf.float32)
+    opt = tf.keras.optimizers.Adam(learning_rate=5, beta_1=0.99, epsilon=1e-1)
+
+    best_loss, best_img = float('inf'), None
+
+    loss_weights = (style_weight, content_weight)
+    cfg = {
+        'model': model,
+        'loss_weights': loss_weights,
+        'init_image': init_image,
+        'gram_style_features': gram_style_features,
+        'content_features': content_features
+    }
+
+    norm_means = np.array([103.939, 116.779, 123.68])
+    min_vals = -norm_means
+    max_vals = 255 - norm_means
+
+    import tqdm
+    for i in tqdm.tqdm(range(num_iterations)):
+        grads, all_loss = compute_grads(cfg)
+        loss, style_score, content_score = all_loss
+        opt.apply_gradients([(grads, init_image)])
+        clipped = tf.clip_by_value(init_image, min_vals, max_vals)
+        init_image.assign(clipped)
+
+        if loss < best_loss:
+            best_loss = loss
+            best_img = deprocess_image(init_image.numpy())
+
+    return best_img, best_loss
+
+content_path = 'doggo.jpg'
+style_path = 'catto.jpg'
+
+best_img, best_loss = run_style_transfer(content_path, style_path)
+plt.imshow(best_img)
+plt.title('Output Image')
+plt.show()
